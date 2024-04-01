c93f898faf1c3cad1c9013164daae1d21dc368a9
muhamash <ashraful@xpeedlab.tech>
2024-04-02 02:25:28 +0600

    search bar -> dev

--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -1,33 +1,46 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-export default function NewsItems({title, text, time, imageSource, author,}) {
+export default function NewsItems ( { title, text, time, imageSource, author, } )
+{
+    const formattedTime = new Date(time).toLocaleString('en-US', {
+  timeZone: 'Asia/Dhaka',
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: true // Use true for 12-hour format or false for 24-hour format
+});
+
+
+
     return (
         <div className='col-span-12 grid grid-cols-12 gap-4'>
-            {/* info */}
+            {/* info */ }
             <div className="col-span-12 lg:col-span-4">
                 <a href="#"
                 ><h3
                     className="mb-2.5 text-2xl font-bold lg:text-[28px]"
                 >
-                        {title}
+                        { title }
                     </h3></a>
                 <p className="text-base text-[#5C5955]">
-                    {text}
+                    { text }
                 </p>
                 <p className="mt-5 text-base text-[#5C5955]">
-                    {time}
+                    { formattedTime }
                 </p>
             </div>
-            {/* thumb */}
+            {/* thumb */ }
             <div className="col-span-12 lg:col-span-8">
                 <img
                     className="w-full"
-                    src={imageSource}
+                    src={ imageSource }
                     alt="thumb"
                 />
                 <p className="mt-5 text-base text-[#5C5955]">
-                    {author}
+                    { author }
                 </p>
             </div>
         </div>
